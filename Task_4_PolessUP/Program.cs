4b3bdc19e8f780dd9fb4ade44b9868f767a1dfd2
CyberSanyok <sashad92175@gmail.com>
2023-04-06 13:05:03 +0300

    Добавьте файлы проекта.

---
+++ Task_4_PolessUP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int[] ints = new int[10] {10,2,3,4,8,6,7,5,9,1};
+        Array.Sort(ints);
+        if (ints.Length == 1) Console.WriteLine(ints[0]);
+        if(ints.Length==2) Console.WriteLine(ints[1]);
+        Console.WriteLine(string.Join(", ",ints));
+        Console.WriteLine(ints[ints.Length-3]);
+    }
+}
